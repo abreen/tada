2bd7cb4aa474fd64a76dba95b60716a83045ac97
Alex Breen <github@abreen.email>
2025-09-10 15:42:25 -0400

    Fix

--- webpack/config.prod.js
+++ webpack/config.prod.js
@@ -10,42 +10,44 @@ const { getProdSiteVariables } = require("./site-variables");
 const distDir = getDistDir();
 const siteVariables = getProdSiteVariables();
 
-module.exports = {
-  mode: "production",
-  entry: { index: "./src/index.ts" },
-  output: { path: distDir, filename: "bundle.js" },
-  resolve: { extensions: [".ts", ".js", ".json"] },
-  devtool: false,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: { loader: "babel-loader" },
-      },
-      { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
+module.exports = async () => {
+  return {
+    mode: "production",
+    entry: { index: "./src/index.ts" },
+    output: { path: distDir, filename: "bundle.js" },
+    resolve: { extensions: [".ts", ".js", ".json"] },
+    devtool: false,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: { loader: "babel-loader" },
+        },
+        { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({ terserOptions: { output: { comments: false } } }),
+      ],
+    },
+    plugins: [
+      ...(await createHtmlPlugins(siteVariables)),
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: [path.join(distDir, "./**/*")],
+      }),
+      new webpack.DefinePlugin({}),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+      new CopyPlugin({ patterns: [{ from: "public", to: "." }] }),
     ],
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({ terserOptions: { output: { comments: false } } }),
-    ],
-  },
-  plugins: [
-    ...createHtmlPlugins(siteVariables),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [path.join(distDir, "./**/*")],
-    }),
-    new webpack.DefinePlugin({}),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-    new CopyPlugin({ patterns: [{ from: "public", to: "." }] }),
-  ],
-  stats: "minimal",
+    stats: "minimal",
+  };
 };
